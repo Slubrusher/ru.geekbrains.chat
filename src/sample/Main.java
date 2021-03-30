dde10b5dfeeffb63caa9120a517411be1aa900b4
IshchukAndrew <ishchukandrew@yandex.ru>
2021-03-30 20:39:44 +0700

    GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +14,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Slubrusher's chat");
+        primaryStage.setScene(new Scene(root, 500, 750));
+        try {
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/Chat icon.jpeg")));
+        } catch (Exception e) {
+            System.out.println("Не удалось найти иконку");
+        }
         primaryStage.show();
     }
 
